cce0df46af5c9194b89a635a157a096c67f23026
s22540 <s22540@pjwstk.edu.pl>
2024-06-25 19:10:01 +0200

    Dodaj pliki projektów.

---
+++ Context/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mvc.Models;
+
+namespace MvcMovie.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
